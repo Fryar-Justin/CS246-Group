40eb6c28be482ca0f8044aee10ece51fa9a8cb07
Fryar-Justin <fry06001@byui.edu>
2018-02-20 19:17:17 -0700

    making a conflict

--- CS246FinalProject/app/src/main/java/me/bradley/cs246finalproject/MainActivity.java
+++ CS246FinalProject/app/src/main/java/me/bradley/cs246finalproject/MainActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // note added by Chris. practicing git push from android studio
+    // note added by Chris. Justin made these edits
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
-// note added by Justin
+// Justin made changes
