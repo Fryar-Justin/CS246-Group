54d01b11884577d55ba250f4eb1f9b077c081590
Chris <pet15056@byui.edu>
2018-02-20 21:16:36 -0500

    Took out comments

--- CS246FinalProject/app/src/main/java/me/bradley/cs246finalproject/MainActivity.java
+++ CS246FinalProject/app/src/main/java/me/bradley/cs246finalproject/MainActivity.java
@@ -4,12 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    // note added by Chris. practicing git push from android studio
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
-// note added by Justin
