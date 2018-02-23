dd1ca54ff8938ea2997266ee270f23ec14ab8c43
BradleyJohnston <bradjohnstonnew@gmail.com>
2018-02-22 18:58:47 -0700

    Design for main activity, and created homeActivity

--- CS246FinalProject/app/src/main/java/me/bradley/cs246finalproject/MainActivity.java
+++ CS246FinalProject/app/src/main/java/me/bradley/cs246finalproject/MainActivity.java
@@ -1,7 +1,9 @@
 package me.bradley.cs246finalproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NumberPicker numberPickerP = (NumberPicker) findViewById(R.id.numberPickerProton);
+        NumberPicker numberPickerN = (NumberPicker) findViewById(R.id.numberPickerNuetron);
+        NumberPicker numberPickerE = (NumberPicker) findViewById(R.id.numberPickerElectron);
+
+        numberPickerP.setMinValue(0);
+        numberPickerP.setMaxValue(150);
+        numberPickerP.setValue(0);
+
+        numberPickerN.setMinValue(0);
+        numberPickerN.setMaxValue(150);
+        numberPickerN.setValue(0);
+
+        numberPickerE.setMinValue(0);
+        numberPickerE.setMaxValue(150);
+        numberPickerE.setValue(0);
+    }
+
+    public void onSubmit(View view) {
+        NumberPicker numberPickerP = (NumberPicker) findViewById(R.id.numberPickerProton);
+        NumberPicker numberPickerN = (NumberPicker) findViewById(R.id.numberPickerNuetron);
+        NumberPicker numberPickerE = (NumberPicker) findViewById(R.id.numberPickerElectron);
+
+        numberPickerP.setMinValue(0);
+        numberPickerP.setMaxValue(150);
+        numberPickerP.setValue(0);
+
+        numberPickerN.setMinValue(0);
+        numberPickerN.setMaxValue(150);
+        numberPickerN.setValue(0);
+
+        numberPickerE.setMinValue(0);
+        numberPickerE.setMaxValue(150);
+        numberPickerE.setValue(0);
     }
 }
 // this is merged
