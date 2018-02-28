7cc1642fedc1fdaacd4a4308eb2f6ab7f87fa02f
Chris <pet15056@byui.edu>
2018-02-27 20:56:30 -0500

    Adding tests to UnitTest with mock

--- CS246FinalProject/app/src/test/java/me/bradley/cs246finalproject/UnitTest.java
+++ CS246FinalProject/app/src/test/java/me/bradley/cs246finalproject/UnitTest.java
@@ -4,8 +4,12 @@ package me.bradley.cs246finalproject;
  * Created by Chris on 2/27/2018.
  */
 
+import android.content.Context;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.regex.Pattern;
 
@@ -13,12 +17,24 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
+@RunWith(MockitoJUnitRunner.class)
 public class UnitTest {
+
+    @Mock
+    Element testElement = mock(Element.class);
 
     @Test
     public void protonsEqualDesiredAtomTest() {
-        assertEquals(MainActivity.getProtons(), 0);
+        // this method requires methods to be static.
+        //   using mock does not require it to be static.
+        //assertEquals(MainActivity.getProtons(), 0);
+    }
+
+    @Test
+    public void protEqualDesiredAtomTest() {
+        assertEquals(testElement.getProtons(), 0);
     }
 
 }
