05ad0c9631d3ecba02249b5127d62118d3f52a35
Chris <pet15056@byui.edu>
2018-02-27 20:20:40 -0500

    Added UnitTest, adjusted build.gradle dependencies.

--- CS246FinalProject/app/src/main/java/me/bradley/cs246finalproject/MainActivity.java
+++ CS246FinalProject/app/src/main/java/me/bradley/cs246finalproject/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int protons = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static int getProtons(){
+        return protons;
     }
 }
