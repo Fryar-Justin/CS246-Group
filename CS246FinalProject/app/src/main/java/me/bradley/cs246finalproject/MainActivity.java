fc0fdeca8772bbe9c885dcd6d3c4dc725508c4f1
BradleyJohnston <bradjohnstonnew@gmail.com>
2018-03-01 18:59:51 -0700

    Main Activity updated

--- CS246FinalProject/app/src/main/java/me/bradley/cs246finalproject/MainActivity.java
+++ CS246FinalProject/app/src/main/java/me/bradley/cs246finalproject/MainActivity.java
@@ -1,5 +1,6 @@
 package me.bradley.cs246finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected NumberPicker numberPickerP;
     protected NumberPicker numberPickerN;
     protected NumberPicker numberPickerE;
+
+    protected Element actualElement;
+    protected Element expectedElement;
+
+    public static final String ELECTRON = "ELECTRON_EXTRA";
+    public static final String NEUTRON = "NEUTRON_EXTRA";
+    public static final String PROTON = "PROTON_EXTRA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,21 +43,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onSubmit(View view) {
+        int proton = 0;
+        int electron = 0;
+        int neutron = 0;
 
         numberPickerP = (NumberPicker) findViewById(R.id.protonNumberPicker);
         numberPickerN = (NumberPicker) findViewById(R.id.nuetronNumberPicker);
         numberPickerE = (NumberPicker) findViewById(R.id.electronNumberPicker);
 
-        numberPickerP.setMinValue(0);
-        numberPickerP.setMaxValue(150);
-        numberPickerP.setValue(0);
+        electron = (numberPickerE.getValue());
+        neutron = (numberPickerN.getValue());
+        proton = (numberPickerP.getValue());
 
-        numberPickerN.setMinValue(0);
-        numberPickerN.setMaxValue(150);
-        numberPickerN.setValue(0);
+        Intent intent = new Intent(this, ElementActivity.class);
 
-        numberPickerE.setMinValue(0);
-        numberPickerE.setMaxValue(150);
-        numberPickerE.setValue(0);
+        intent.putExtra(ELECTRON, electron);
+        intent.putExtra(NEUTRON, neutron);
+        intent.putExtra(PROTON, proton);
+
+        startActivity(intent);
     }
 }
